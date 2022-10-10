7efbc5741bf70e43f9c1ce69fbe6bd55f5d5dad2
Gutu Daniel Geleta <gutudanielgeleta@gmail.com>
2022-10-10 10:28:29 +0300

    Update fizzbuzz(leetcode).java

--- fizzbuzz(leetcode).java
+++ fizzbuzz(leetcode).java
@@ -7,7 +7,7 @@ class Solution {
         List<String> fizz = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                fizz.add("Fizz Buzz");
+                fizz.add("FizzBuzz");
             } else if (i % 3 == 0) {
                 fizz.add("Fizz");
             } else if (i % 5 == 0) {
